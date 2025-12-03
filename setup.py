c2a1352ca5e5c838b1a32bc31c3b2ae8b5e59c1d
Racoon-GIT <119979618+Racoon-GIT@users.noreply.github.com>
2025-12-03 02:07:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,7 @@ setup(
     install_requires=[
         'flask>=3.0.0',
         'requests>=2.31.0',
-        'lxml>=5.1.0',
-        'openpyxl>=3.1.2',
         'gunicorn>=21.2.0',
-        'python-dateutil>=2.8.2',
     ],
     python_requires='>=3.11',
 )
